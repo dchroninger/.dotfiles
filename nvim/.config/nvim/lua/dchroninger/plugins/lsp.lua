6efed5be4a4c02240c1ec11172707bb32ab13fcb
Dave Chroninger <david.a.chroninger@gmail.com>
2023-06-09 10:14:04 -0700

    Added 'require' to globals

--- nvim/.config/nvim/lua/dchroninger/plugins/lsp.lua
+++ nvim/.config/nvim/lua/dchroninger/plugins/lsp.lua
@@ -9,31 +9,34 @@ lsp.ensure_installed({
 
 -- Fix Undefined global 'vim'
 lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {
+          'vim',
+          'require'
         }
+      }
     }
+  }
 })
 
 lsp.configure('tailwindcss-language-server', {
-    settings = {
-        tailwindcss = {
-            experimental = {
-                classRegex = {
-                    { "cva\\(([^)]*)\\)",
-                    "[\"'`]([^\"'`]*).*?[\"'`]" },
-                }
-            }
+  settings = {
+    tailwindcss = {
+      experimental = {
+        classRegex = {
+          { "cva\\(([^)]*)\\)",
+            "[\"'`]([^\"'`]*).*?[\"'`]" },
         }
+      }
     }
+  }
 })
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -49,17 +52,17 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -76,6 +79,5 @@ end)
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
-
