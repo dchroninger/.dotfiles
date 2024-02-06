9a8f6e20bcae5a16029fdb3ec1b1ee353aa839bc
Dave Chroninger <52579889+dchroninger@users.noreply.github.com>
2024-02-05 19:25:29 -0800

    Removed 'help' from treesitter

--- nvim/.config/nvim/lua/dchroninger/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/dchroninger/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "go", "lua", "vim", "help", "rust", "json", "javascript", "typescript", "tsx", "yaml", "html", "css", "markdown", "markdown_inline", "dockerfile", "gitignore" },
+  ensure_installed = { "c", "go", "lua", "vim", "rust", "json", "javascript", "typescript", "tsx", "yaml", "html", "css", "markdown", "markdown_inline", "dockerfile", "gitignore" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
