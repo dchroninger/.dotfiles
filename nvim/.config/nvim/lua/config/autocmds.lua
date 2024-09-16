7229c086d74709788f89304593a70dbfe64f8d3a
Dave Chroninger <52579889+dchroninger@users.noreply.github.com>
2024-09-15 23:34:39 -0700

    Updated neovim config

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.conceallevel = 1
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.gohtml,*.gotmpl,*.html",
+  callback = function()
+    if vim.fn.search("{{.\\+}}", "nw") ~= 0 then
+      local buf = vim.api.nvim_get_current_buf()
+      vim.api.nvim_buf_set_option(buf, "filetype", "gotmpl")
+      vim.api.nvim_buf_set_option(buf, "filetype", "html")
+    end
+  end,
+})
--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,86 +1,92 @@
 return {
-  "epwalsh/obsidian.nvim",
-  lazy = true,
-  event = {
-    "BufReadPre " .. vim.fn.expand("~") .. "/notes/personal/**.md",
-    "BufReadPre " .. vim.fn.expand("~") .. "/notes/work/**.md",
-  },
+	"epwalsh/obsidian.nvim",
+	lazy = true,
+	event = {
+		"BufReadPre " .. vim.fn.expand("~") .. "/kb/**.md",
+	},
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-    "nvim-telescope/telescope.nvim",
-  },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+		"nvim-telescope/telescope.nvim",
+	},
 
-  opts = {
-    workspaces = {
-      { name = "personal", path = vim.fn.expand("~") .. "/notes/personal" },
-      { name = "work", path = vim.fn.expand("~") .. "/notes/work" },
-    },
+	opts = {
+		workspaces = {
+			{ name = "kb", path = vim.fn.expand("~") .. "/kb" },
+		},
 
-    daily_notes = {
-      folder = "daily",
-    },
+		daily_notes = {
+			folder = "notes/daily",
+			template = "daily",
+			alias_format = "%A %B %d, %Y",
+		},
 
-    notes_subdir = "notes",
+		templates = {
+			subdir = "templates",
+			date_format = "%Y-%m-%d-%a",
+			time_format = "%H:%M",
+		},
 
-    completion = {
-      nvim_cmp = true,
-      min_chars = 2,
-      new_notes_location = "notes_subdir",
-    },
+		notes_subdir = "notes",
+		new_notes_location = "notes_subdir",
 
-    follow_url_func = function(url)
-      vim.fn.jobstart({ "open", url })
-    end,
-  },
+		completion = {
+			nvim_cmp = true,
+			min_chars = 2,
+		},
 
-  config = function(_, opts)
-    require("obsidian").setup(opts)
+		follow_url_func = function(url)
+			vim.fn.jobstart({ "open", url })
+		end,
+	},
 
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "markdown", "markdown_inline" },
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { "markdown" },
-      },
-    })
+	config = function(_, opts)
+		require("obsidian").setup(opts)
 
-    vim.keymap.set("n", "gf", function()
-      if require("obsidian").util.cursor_on_markdown_link() then
-        return "<cmd>ObsidianFollowLink<CR>"
-      else
-        return "gf"
-      end
-    end, { noremap = false, expr = true })
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "markdown", "markdown_inline" },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { "markdown" },
+			},
+		})
 
-    vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>")
-    vim.keymap.set("n", "<leader>od", "<cmd>ObsidianToday<CR>")
-    vim.keymap.set("n", "<leader>of", "<cmd>ObsidianQuickSwitch<CR>")
-    vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>")
-  end,
+		vim.keymap.set("n", "gf", function()
+			if require("obsidian").util.cursor_on_markdown_link() then
+				return "<cmd>ObsidianFollowLink<CR>"
+			else
+				return "gf"
+			end
+		end, { noremap = false, expr = true })
 
-  note_frontmatter_func = function(note)
-    local out = { aliases = note.aliases, tags = note.tags }
-    if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-      for k, v in pairs(note.metadata) do
-        out[k] = v
-      end
-    end
-    return out
-  end,
+		vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>")
+		vim.keymap.set("n", "<leader>od", "<cmd>ObsidianToday<CR>")
+		vim.keymap.set("n", "<leader>of", "<cmd>ObsidianQuickSwitch<CR>")
+		vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>")
+		vim.keymap.set("n", "<leader>or", "<cmd>ObsidianRename<CR>")
+		vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>")
+		vim.keymap.set("n", "<leader>oT", "<cmd>ObsidianNewFromTemplate<CR>")
+	end,
 
-  folow_url_func = function(url)
-    vim.fn.jobstart({ "open", url })
-  end,
+	note_frontmatter_func = function(note)
+		local out = { aliases = note.aliases, tags = note.tags }
+		if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+			for k, v in pairs(note.metadata) do
+				out[k] = v
+			end
+		end
+		return out
+	end,
 
-  templates = {
-    subdir = "templates",
-    date_format = "%Y-%m-%d-%a",
-    time_format = "%H:%M",
-  },
+	folow_url_func = function(url)
+		vim.fn.jobstart({ "open", url })
+	end,
 
-  open_app_foreground = false,
-  finder = "telescope.nvim",
+	--  TODO: Add function to create meeting note
+	--  TODO: Add function to create class note
+
+	open_app_foreground = false,
+	finder = "telescope.nvim",
 }
